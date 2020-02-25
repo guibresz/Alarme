9781bcacc02e4b023e38f76726e6021ed4c761b3
guibresz <50353802+guibresz@users.noreply.github.com>
2020-02-25 12:00:28 +0100

    import inutile retiré

--- PremiereInterface/app/src/main/java/com/example/premiereinterface/CFichier.java
+++ PremiereInterface/app/src/main/java/com/example/premiereinterface/CFichier.java
@@ -1,25 +1,12 @@
 package com.example.premiereinterface;
 
 import android.content.Context;
-import android.media.audiofx.EnvironmentalReverb;
-import android.os.Environment;
-import android.widget.Toast;
-
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
+
 
 public class CFichier {
 
